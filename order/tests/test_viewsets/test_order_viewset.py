17fb09441871b10c4a0340546f746b46e8eee4d7
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2025-02-14 16:06:38 -0300

    feat: test order auth

--- order/tests/test_viewsets/test_order_viewset.py
+++ order/tests/test_viewsets/test_order_viewset.py
@@ -3,6 +3,7 @@ import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient, APITestCase
+from rest_framework.authtoken.models import Token
 
 from order.factories import OrderFactory, UserFactory
 from order.models import Order
@@ -14,6 +15,10 @@ class TestOrderViewSet(APITestCase):
     client = APIClient()
 
     def setUp(self):
+        self.user = UserFactory() # create user
+        token = Token.objects.create(user=self.user) # create token
+        token.save() # save token
+
         self.category = CategoryFactory(title="technology")
         self.product = ProductFactory(
             title="mouse", price=100, category=[self.category]
@@ -21,6 +26,9 @@ class TestOrderViewSet(APITestCase):
         self.order = OrderFactory(product=[self.product])
 
     def test_order(self):
+        token = Token.objects.get(user__username=self.user.username)  # get user token
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + token.key)  # credentials
         response = self.client.get(
             reverse("order-list", kwargs={"version": "v1"}))
 
@@ -42,9 +50,12 @@ class TestOrderViewSet(APITestCase):
         )
 
     def test_create_order(self):
-        user = UserFactory()
+        token = Token.objects.get(user__username=self.user.username)  # get user token
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + token.key)  # credentials
+        
         product = ProductFactory()
-        data = json.dumps({"products_id": [product.id], "user": user.id})
+        data = json.dumps({"products_id": [product.id], "user": self.user.id})
 
         response = self.client.post(
             reverse("order-list", kwargs={"version": "v1"}),
@@ -54,10 +65,13 @@ class TestOrderViewSet(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-        created_order = Order.objects.get(user=user)
+        created_order = Order.objects.get(user=self.user)
 
 # test: get order by id
     def test_get_single_order(self):
+        token = Token.objects.get(user__username=self.user.username)  # get user token
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + token.key)  # credentials
         response = self.client.get(
             reverse("order-detail", kwargs={"version": "v1", "pk": self.order.id})
         )
@@ -69,6 +83,9 @@ class TestOrderViewSet(APITestCase):
 
 # test: remove order by id
     def test_delete_order(self):
+        token = Token.objects.get(user__username=self.user.username)  # get user token
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + token.key)  # credentials
         response = self.client.delete(
             reverse("order-detail", kwargs={"version": "v1", "pk": self.order.id})
         )

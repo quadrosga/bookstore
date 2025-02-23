956aec307eab557a8fccc85b4b0985172e5320ea
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2025-02-23 20:59:45 -0300

    chore: update secret key

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -148,7 +148,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret-key")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 

eccba72a50648d0b92239e4a8e74f2abd35c8dc6
Gabriela Quadros_do_Amaral <quadros.ga@hotmail.com>
2025-02-27 16:57:00 -0300

    chore: add debugging logs

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,27 +1,21 @@
-from django.http import HttpResponse
-from django.template import loader
-from django.views.decorators.csrf import csrf_exempt
-
-import git
-
-
 @csrf_exempt
 def update(request):
     if request.method == "POST":
-        '''
-        pass the path of the diectory where your project will be
-        stored on PythonAnywhere in the git.Repo() as parameter.
-        Here the name of my directory is "test.pythonanywhere.com"
-        '''
-        repo = git.Repo('/home/quadrosga/bookstore')
-        origin = repo.remotes.origin
+        try:
+            repo = git.Repo('/home/quadrosga/bookstore')
+            origin = repo.remotes.origin
+            pull_info = origin.pull()
+            
+            # Debugging: Log what was pulled
+            pulled_commits = [commit.hexsha for commit in pull_info]
+            log_message = f"Pulled commits: {pulled_commits}"
 
-        origin.pull()
-        return HttpResponse("Updated code on PythonAnywhere")
+            # Force reload on PythonAnywhere
+            import os
+            os.system("touch /var/www/quadrosga_pythonanywhere_com_wsgi.py")
+
+            return HttpResponse(f"Updated code on PythonAnywhere. {log_message}")
+        except Exception as e:
+            return HttpResponse(f"Error updating: {str(e)}", status=500)
     else:
-        return HttpResponse("Couldn't update the code on PythonAnywhere")
-
-
-def hello_world(request):
-  template = loader.get_template('hello_world.html')
-  return HttpResponse(template.render())
+        return HttpResponse("Couldn't update the code on PythonAnywhere", status=400)
